ec5b7df9ba13dfb247340ad7502823abf6c24535
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-09-18 16:56:27 +1000

    Apoli: Update AddVelocityAction (bi-entity).

--- src/main/java/io/github/edwinmindcraft/apoli/common/action/bientity/AddVelocityAction.java
+++ src/main/java/io/github/edwinmindcraft/apoli/common/action/bientity/AddVelocityAction.java
@@ -5,8 +5,11 @@ import io.github.edwinmindcraft.apoli.api.power.factory.BiEntityAction;
 import io.github.edwinmindcraft.apoli.common.action.configuration.AddBiEntityVelocityConfiguration;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import org.apache.logging.log4j.util.TriConsumer;
 import org.joml.Vector3f;
+
+import java.util.function.Consumer;
 
 public class AddVelocityAction extends BiEntityAction<AddBiEntityVelocityConfiguration> {
 	public AddVelocityAction() {
@@ -17,10 +20,10 @@ public class AddVelocityAction extends BiEntityAction<AddBiEntityVelocityConfigu
 	public void execute(AddBiEntityVelocityConfiguration configuration, Entity actor, Entity target) {
 		if (target instanceof Player && (target.level().isClientSide() ? !configuration.client() : !configuration.server()))
 			return;
-		Vector3f vec = configuration.direction();
-		TriConsumer<Float, Float, Float> method = configuration.set() ? target::setDeltaMovement : target::push;
-		Space.transformVectorToBase(target.position().subtract(actor.position()), vec, actor.getYRot(), true); // vector normalized by method
-		method.accept(vec.x(), vec.y(), vec.z());
+		Vector3f vec = new Vector3f(configuration.direction());
+        Consumer<Vec3> method = configuration.set() ? target::setDeltaMovement : target::addDeltaMovement;
+        Space.transformVectorToBase(target.position().subtract(actor.position()), vec, actor.getYRot(), true); // vector normalized by method
+        method.accept(new Vec3(vec));
 		target.hurtMarked = true;
 	}
 }
