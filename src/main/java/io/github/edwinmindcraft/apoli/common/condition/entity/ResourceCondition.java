1bb4b86989723ba78662513ed42ba5b49ce46dcc
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-09-27 04:44:37 +1000

    Apoli: Fix EdwinMindcraft/origins-architectury#372

--- src/main/java/io/github/edwinmindcraft/apoli/common/condition/entity/ResourceCondition.java
+++ src/main/java/io/github/edwinmindcraft/apoli/common/condition/entity/ResourceCondition.java
@@ -4,6 +4,7 @@ import io.github.edwinmindcraft.apoli.api.component.IPowerContainer;
 import io.github.edwinmindcraft.apoli.api.power.factory.EntityCondition;
 import io.github.edwinmindcraft.apoli.common.condition.configuration.ResourceComparisonConfiguration;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 
 import java.util.OptionalInt;
@@ -17,8 +18,8 @@ public class ResourceCondition extends EntityCondition<ResourceComparisonConfigu
 	@Override
 	public boolean check(ResourceComparisonConfiguration configuration, Entity entity) {
 		return IPowerContainer.get(entity).resolve().map(x -> x.getPower(configuration.resource().power())).map(power -> {
-			if (entity instanceof Player player) {
-				OptionalInt value = power.value().getValue(player);
+			if (entity instanceof LivingEntity living) {
+				OptionalInt value = power.value().getValue(living);
 				return value.isPresent() && configuration.comparison().check(value.getAsInt());
 			}
 			return false;
