5591a7d71210fa51c1610107e16fd243ccc6a493
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-05 10:51:34 +1100

    Apoli: Fix EdwinMindcraft/origins-architectury#391

--- src/main/java/io/github/edwinmindcraft/apoli/common/action/bientity/SimpleBiEntityAction.java
+++ src/main/java/io/github/edwinmindcraft/apoli/common/action/bientity/SimpleBiEntityAction.java
@@ -2,14 +2,10 @@ package io.github.edwinmindcraft.apoli.common.action.bientity;
 
 import io.github.edwinmindcraft.apoli.api.configuration.NoConfiguration;
 import io.github.edwinmindcraft.apoli.api.power.factory.BiEntityAction;
-import io.github.edwinmindcraft.apoli.common.ApoliCommon;
-import io.github.edwinmindcraft.apoli.common.network.S2CPlayerMount;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.network.PacketDistributor;
 
 import java.util.function.BiConsumer;
 
@@ -17,9 +13,12 @@ public class SimpleBiEntityAction extends BiEntityAction<NoConfiguration> {
 
 	public static void mount(Entity actor, Entity target) {
 		actor.startRiding(target, true);
-		if (!actor.level.isClientSide() && target instanceof ServerPlayer player) {
+		// The below is unnecessary as the client will execute the above before the server.
+		/*
+		if (!actor.level().isClientSide() && target instanceof ServerPlayer player) {
 			ApoliCommon.CHANNEL.send(PacketDistributor.PLAYER.with(() -> player), new S2CPlayerMount(actor.getId(), target.getId()));
 		}
+		 */
 	}
 
 	public static void setInLove(Entity actor, Entity target) {
