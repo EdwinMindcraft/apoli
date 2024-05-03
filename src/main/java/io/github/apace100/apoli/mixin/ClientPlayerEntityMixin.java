dd00e6200b4d4ebad93494c9495f5de73ea96b67
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-05-03 16:38:30 +1000

    Apoli: Fix ClientPlayerEntityMixin constructor.

--- src/main/java/io/github/apace100/apoli/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/io/github/apace100/apoli/mixin/ClientPlayerEntityMixin.java
@@ -8,21 +8,26 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.player.Abilities;
+import net.minecraft.world.entity.player.ProfilePublicKey;
 import net.minecraftforge.common.util.LazyOptional;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
+
 @Mixin(LocalPlayer.class)
 public abstract class ClientPlayerEntityMixin extends AbstractClientPlayer implements WaterMovingEntity {
 
+	@Unique
 	private boolean isMoving = false;
 
-	public ClientPlayerEntityMixin(ClientLevel world, GameProfile profile) {
-		super(world, profile);
+	public ClientPlayerEntityMixin(ClientLevel world, GameProfile profile, @Nullable ProfilePublicKey key) {
+		super(world, profile, key);
 	}
 
 	@Inject(at = @At("HEAD"), method = "isUnderWater", cancellable = true)
